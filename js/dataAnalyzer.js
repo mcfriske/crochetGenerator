77f200e0f150076e9609c896912fce544c823aaf
Mikhaila Friske <frisk028@umn.edu>
2019-08-05 19:07:17 -0600

    update for time functions to work

--- js/dataAnalyzer.js
+++ js/dataAnalyzer.js
@@ -1,69 +1,196 @@
-function slugify(string) {
-  return string.toLowerCase().replace(/\s/g, '-').replace(/[^a-z0-9-]/g, '');
-}
-
-var typeOptions = {
-  'number': ['default', 'tens', 'hundreds', 'thousands'],
-  'date': ['day', 'month', 'year'],
-  'time': ['default', 'minute', '15 minute', 'half hour', 'hour']
-}
-
-var knownTypes = {
-  'bpm': 'number',
-  'heart-rate': 'number',
-  'heartrate': 'number',
-  'time': 'time',
-  'date': 'date'
-}
-
-function DataAnalyzer(csvInputID, settingsFormID) {
-  this.csvInput = document.getElementById(csvInputID);
-  this.settingsForm = document.getElementById(settingsFormID);
+function DataAnalyzer(crochetObject, pagination) {
+  this.csvInput = document.getElementById('csv-data');
+  this.settingsForm = document.getElementById('settingsForm');
+  this.$groupOrder = document.getElementsByName('groupOrder');
+  this.$stitches = document.getElementsByName('stitch');
+  this.currentTab = 0;
+  this.columnOptions = []
+  this.groupingOptions = ['default']
   this.rawData = {};
   this.organizedData = {};
+  this.crochetObject = crochetObject;
+  this.pagination = pagination;
+  this.keyType = null;
+  this.functionType = null;
+  this.timeFormat = null;
+  this.dateFormat = null;
 
   this.init();
-
-  // console.log(this.csvInput, this.logicInput);
 }
 
 DataAnalyzer.prototype = {
   init: function() {
-    this.setupHandlers().setupEvents();
+    this.setupHandlers().setupEvents().updateUncertaintyPreview();
   },
 
   setupHandlers: function() {
+    // Form Handlers
     this.csvChangeHandler = this.updateCSVData.bind(this);
-    this.settingsChangeHandler = this.updateGrouping.bind(this);
+    this.groupColumnsChangeHandler = this.updateGroupingOptions.bind(this);
+    this.functionColumnsChangeHandler = this.updateFunctionOptions.bind(this);
+    this.uncertaintyPreviewHandler = this.updateUncertaintyPreview.bind(this);
+    this.stitchUpdateHandler = this.stringToRegex.bind(this);
+
+    // Data Key Grouping Handlers
+    this.numberKeyHandler = this.numberKey.bind(this);
+    this.timeKeyHandler = this.timeKey.bind(this);
+    this.dateKeyHandler = this.dateKey.bind(this);
+    // Value Groupings
+    this.numberValueHandler = this.numberValue.bind(this);
+    this.timeValueHandler = this.timeValue.bind(this);
+    // this.dateValueHandler = this.dateValue.bind(this);
+
+    // Sorting
+    this.sortKeysHandler = this.sortKeys.bind(this);
     return this;
   },
 
   setupEvents: function() {
     this.csvInput.addEventListener('change', this.csvChangeHandler);
-    this.settingsForm.addEventListener('change', this.settingsChangeHandler);
+    this.settingsForm.groupColumns.addEventListener('change', this.groupColumnsChangeHandler);
+    this.settingsForm.functionColumns.addEventListener('change', this.functionColumnsChangeHandler);
+    this.settingsForm.uncertaintyRange.addEventListener('input', this.uncertaintyPreviewHandler);
+    [].forEach.call(this.$groupOrder,function(e){e.addEventListener('change',this.sortKeysHandler,false)}.bind(this));
+    [].forEach.call(this.$stitches,function(e){e.addEventListener('change',this.stitchUpdateHandler,false)}.bind(this));
     return this;
   },
 
+  /////// FORM UPDATES
   updateCSVData: function(e) {
-    console.log('alert');
     this.rawData = d3.csvParse(this.csvInput.value);
 
     this.dataLength = this.rawData.length;
+    this.updateColumnOptions();
+    this.updateGroupingOptions();
+    this.updateFunctionOptions();
 
   },
 
-  updateGrouping: function(e) {
-    console.log('here');
+  updateUncertaintyPreview: function() {
+    var span = document.getElementById('demoUncertainty');
+
+    span.innerText = this.settingsForm.uncertaintyRange.value;
+  },
+
+  updateColumnOptions: function(e) {
+    var selects = this.settingsForm.columnOptions;
+
+    for (var i = 0; i < selects.length; i++) {
+      var columns = selects[i];
+      while(columns.firstChild) {
+        columns.removeChild(columns.firstChild);
+      }
+
+      for (var index in this.rawData.columns) {
+        var opt = document.createElement('option');
+        var col = this.rawData.columns[index];
+        opt.value = slugify(col);
+        opt.textContent = col;
+        columns.appendChild(opt);
+      }
+    }
+    
+  },
+
+  updateGroupingOptions: function() {
+    var grouping = this.settingsForm.groupingOptions;
+    var typeOptions = getKnownTypeOptions(this.settingsForm.groupColumns.value);
+    this.keyType = knownTypes[this.settingsForm.groupColumns.value];
+
+    while(grouping.firstChild) {
+      grouping.removeChild(grouping.firstChild);
+    }
+
+    for (var index in typeOptions) {
+      var opt = document.createElement('option');
+      var typeOpt = typeOptions[index];
+      opt.value = slugify(typeOpt);
+      opt.textContent = typeOpt;
+      grouping.appendChild(opt);
+    }
+  },
+
+  updateFunctionOptions: function() {
+    var functions = this.settingsForm.functionOptions;
+    var funcOptions = getFunctionOptions(this.settingsForm.functionColumns.value);
+    this.functionType = knownTypes[this.settingsForm.functionColumns.value];
+
+    while(functions.firstChild) {
+      functions.removeChild(functions.firstChild);
+    }
+
+    for (var index in funcOptions) {
+      var opt = document.createElement('option');
+      var funcOpt = funcOptions[index];
+      opt.value = slugify(funcOpt);
+      opt.textContent = funcOpt;
+      functions.appendChild(opt);
+    }
+  },
+
+  updateGrouping: function() {
+    var keyHandler, valueHandler, sortHandler;
+    switch(this.keyType) {
+      case 'time':
+        keyHandler = this.timeKeyHandler;
+        break;
+      case 'date':
+        keyHandler = this.dateKeyHandler;
+        break;
+      default:
+        keyHandler = this.numberKeyHandler;
+    }
+
+    switch(this.functionType) {
+      case 'time':
+        valueHandler = this.timeValueHandler;
+        break;
+      case 'date':
+        valueHandler = this.dateValueHandler;
+        break;
+      default:
+        valueHandler = this.numberValueHandler;
+    }
+
+    console.log(this.functionType, this.keyType);
     var grouped = d3.nest()
-                    .key(function(d) {var tens = Math.floor(d["Heart Rate"]/10).toString(); return tens+'0-'+tens+'9';})
-                    .rollup(function(v) { return {
-                        count: v.length,
-                        total: d3.sum(v, function(d) { return d["Heart Rate"]; }),
-                        avg: d3.mean(v, function(d) { return d["Heart Rate"]; })
-                      }; })
-                    .entries(this.rawData);
+                    .key(keyHandler)
+                    .rollup(valueHandler)
+                    .entries(this.rawData)
+                    // .sort(this.sortNumberKeys);
 
     this.organizedData = grouped;
+    this.pagination.setRows(this.organizedData).getPagination();
+  },
+
+  updateDataCrochet: function() {
+    this.updateGrouping();
+  },
+
+  sortKeys: function() {
+    this.organizedData = this.organizedData.sort(this.sortNumberKeys);
+    this.pagination.setRows(this.organizedData).getPagination();
+  },
+
+  getStitchIndex: function(row) {
+    var key, value, weight;
+    var selected = this.crochetObject.selectedGroups.sort(this.sortNumberKeys);
+    var weighted = Math.random() + (this.settingsForm.uncertaintyRange.value / 100);
+
+    if (row <= selected.length) {
+      key = selected[row - 1];
+      [value, weight] = this.organizedData.find(item => item.key === key).value;
+      if (weighted < 1) {
+        return co.getStitchIndex(value);
+
+      } else {
+        return round(random(0,8));
+      }
+    } else {
+      return round(random(0,8));
+    }
+
+    
   },
 
   getRandomInt: function(max) {
@@ -71,21 +198,53 @@ DataAnalyzer.prototype = {
   },
 
   getRandomItemWeighted: function() {
-    var keyToStitches = {"<70": 0, "70-79": 1, "80-89": 3, "90-99": 2, "100-109":6, "110-119":7, "120-129":1, ">129": 0};
     var rand = this.getRandomInt(this.dataLength);
-    var weight = 0;
-    for (var key in this.organizedData) {
-      weight = this.organizedData[key];
-      rand = rand - weight;
-      if (rand <= 0) {
-        console.log(key);
-        return keyToStitches[key];
-      }
+    var key, value, weight;
+    for (var index in this.organizedData) {
+      key = this.organizedData[index].key;
+      [value, weight] = this.organizedData[index].value;
+      console.log(key, value, weight);
+      // weight = this.organizedData[key];
+      // rand = rand - weight;
+      // if (rand <= 0) {
+      //   console.log(key);
+      //   // return keyToStitches[key];
+      // }
     }
   },
 
+  //////// DATA ANALYSIS FUNCTIONS
   numberKey: function(d) {
+    var groupColumn = this.settingsForm.groupColumns.value;
+    var groupingOption = this.settingsForm.groupingOptions.value;
 
+    for (var id in d) {
+      if (slugify(id) == groupColumn) {
+        groupColumn = id;
+        break;
+      }
+    }
+    var value = parseInt(d[groupColumn]);
+    var key = ""
+
+    switch(groupingOption) {
+      case 'tens':
+        var tens = Math.floor(value / 10);
+        key = tens.toString() + "0-" + tens.toString() +"9";
+        break;
+      case 'hundreds':
+        var hund = Math.floor(value / 100);
+        key = hund.toString() + "00-" + hund.toString() +"99";
+        break;
+      case 'thousands':
+        var thous = Math.floor(value / 1000);
+        key = thous.toString() + "000-" + thous.toString() +"999";
+        break;
+      default:
+        key = d[groupColumn];
+    }
+
+    return key;
   },
 
   dateKey: function(d) {
@@ -93,7 +252,237 @@ DataAnalyzer.prototype = {
   },
 
   timeKey: function(d) {
+    var groupColumn = this.settingsForm.groupColumns.value;
+    var groupingOption = this.settingsForm.groupingOptions.value;
 
+    for (var id in d) {
+      if (slugify(id) == groupColumn) {
+        groupColumn = id;
+        break;
+      }
+    }
+    var value = d[groupColumn];
+    if (!this.timeFormat && !value.match(this.timeFormat)) {
+      this.timeFormat = getTimeRegex(value);
+    }
+    var time = moment(value, this.timeFormat);
+    var key = ""
+    var hours, minutes;
+    switch(groupingOption) {
+      case 'minute':
+        hours = time.hours();
+        minutes = time.minutes();
+        minutes = minutes > 9 ? minutes : `0${minutes}`;
+        key = `${hours}:${minutes}`;
+        break;
+      case '15-minutes':
+        hours = time.hours();
+        minutes = Math.floor(time.minutes() / 15) * 15;
+        key = `${hours}:${minutes > 9 ? minutes : '00'}-${hours}:${minutes+14}`;
+        break;
+      case 'half-hour':
+        hours = time.hours();
+        minutes = Math.floor(time.minutes() / 30) * 30;
+        key = `${hours}:${minutes > 9 ? minutes : '00'}-${hours}:${minutes+29}`;
+        break;
+      case 'hour':
+        hours = time.hours();
+        hours = hours > 9 ? hours : `0${hours}`;
+        key = `${hours}:00-${hours}:59`
+        break;
+      default:
+        key = d[groupColumn];
+    }
+
+    return key;
+
+  },
+
+  sortNumberKeys: function(a,b) {
+    if (a instanceof Object) {
+      a = parseInt(a.key.split('-')[0]);
+      b = parseInt(b.key.split('-')[0]);
+    } else {
+      a = parseInt(a.split('-')[0]);
+      b = parseInt(b.split('-')[0]);
+    }
+    var ascending = document.getElementById('ascending').checked;
+
+    if (ascending) {
+      return a < b ? -1 : a === b ? 0 : 1;
+    } else {
+      return a > b ? -1 : a === b ? 0 : 1;
+    }
+  },
+
+  numberValue: function(v) {
+    var functionColumn = this.settingsForm.functionColumns.value;
+    var functionOption = this.settingsForm.functionOptions.value;
+    var columns = this.rawData.columns;
+
+    for (var id in columns) {
+      if (slugify(columns[id]) == functionColumn) {
+        functionColumn = columns[id];
+        break;
+      }
+    }
+
+    switch(functionOption) {
+      case 'unique-count':
+        return [d3.nest()
+                 .key(function(d) {return d[functionColumn]})
+                 .entries(v)
+                 .length, v.length];
+      case 'minimum':
+        return [d3.min(v, function(d) {return parseInt(d[functionColumn])}), v.length];
+      case 'maximum':
+        return [d3.max(v, function(d) {return parseInt(d[functionColumn])}), v.length];
+      case 'average':
+        return [d3.mean(v, function(d) {return d[functionColumn]}), v.length];
+      case 'sum':
+        return [d3.sum(v, function(d) {return d[functionColumn]}), v.length];
+      default: // this is count
+        return [v.length, v.length];
+    }
+  }, 
+
+  timeValue: function(v) {
+    var functionColumn = this.settingsForm.functionColumns.value;
+    var functionOption = this.settingsForm.functionOptions.value;
+    var columns = this.rawData.columns;
+
+    for (var id in columns) {
+      if (slugify(columns[id]) == functionColumn) {
+        functionColumn = columns[id];
+        break;
+      }
+    }
+
+    var value = v[0][functionColumn];
+    if (!this.timeFormat && !value.match(this.timeFormat)) {
+      this.timeFormat = getTimeRegex(value);
+    }
+
+    switch(functionOption) {
+      case 'unique-count':
+        return [d3.nest()
+                 .key(function(d) {return d[functionColumn]})
+                 .entries(v)
+                 .length, v.length];
+      case 'most-frequent-hour':
+        var grouped = d3.nest().key(function(d){return moment(d[functionColumn], this.timeFormat).hours()}.bind(this))
+                        .entries(v)
+        console.log(grouped);
+        return [v.length, v.length];
+      default: // this is count
+        return [v.length, v.length];
+    }
+    
+  },
+
+  stringToRegex: function(e) {
+    var stitchInput = e.currentTarget;
+    var string = stitchInput.value;
+    var items = string.split(',');
+    var stitchID = stitchInput.id;
+    var regexString = "^("
+
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+
+      if (item.indexOf('-') !== -1) {
+        var [start,stop] = item.split('-');
+        start = parseInt(start);
+        stop = parseInt(stop);
+
+        var startTens, stopTens, stopOnes;
+        startTens = Math.floor(start / 10);
+        startOnes = start % 10;
+        stopTens = Math.floor(stop / 10);
+        stopOnes = stop % 10;
+
+        for (var j = startTens; j <= stopTens; j++) {
+          if (j != stopTens) {
+            if (j ===  startTens) {
+              regexString += `${j}[${startOnes}-9]|`;
+            } else {
+              regexString += `${j}[0-9]|`;
+            }
+          }
+          else {
+            if (j === startTens) {
+              regexString += `${j}[${startOnes}-${stopOnes}]|`
+            } else {
+              regexString += `${j}[0-${stopOnes}]|`
+            }
+          }
+        }
+      } else {
+        regexString += item.trim() + '|';
+      }
+    }
+    regexString = regexString.replace(/\|$/g, ')'); // replace last pipe with closing parenthesis
+
+    this.crochetObject.addStitchRegex(stitchID, regexString);
+
+
+  },
+
+
+  /////// MODAL AND TAB CONTROLS
+  openStep: function(e, id, change) {
+    var tabslinks, tabcontent;
+    tablinks = document.getElementsByClassName('step');
+    tabcontent = document.getElementsByClassName('step-content');
+
+    if (id == null) {
+      this.currentTab += change;
+      id = "step" + (this.currentTab+1).toString();
+    } else {
+      this.currentTab = parseInt(id.replace('step', '')) - 1
+    }
+
+    for (var i = 0; i < tabcontent.length; i++) {
+      if (id == tabcontent[i].id){
+        tabcontent[i].classList.remove('hidden');
+      } else {
+        tabcontent[i].classList.add('hidden');
+      }
+      tablinks[i].classList.remove('current');
+    }
+
+    if (e != null && e.currentTarget.tagName != 'BUTTON') {
+      e.currentTarget.classList.add('current');
+    } else {
+      tablinks[this.currentTab].classList.add('current');
+    }
+
+    if (this.currentTab == 0) {
+      document.getElementById('prevButton').classList.add('hidden');
+    } else {
+      document.getElementById('prevButton').classList.remove('hidden');
+    }
+
+    if (this.currentTab != 2) {
+      document.getElementById('goButton').classList.add('hidden');
+    } else {
+      document.getElementById('goButton').classList.remove('hidden');
+    }
+
+    if (this.currentTab == 2) {
+      document.getElementById('nextButton').classList.add('hidden');
+      this.updateDataCrochet();
+    } else {
+      document.getElementById('nextButton').classList.remove('hidden');
+    }
+
+  },
+
+  show: function(id) {
+    document.getElementById(id).classList.toggle('hidden');
+    this.openStep(null, 'step1', null);
   }
+
+
 
 }
--- js/dataCrochet.js
+++ js/dataCrochet.js
@@ -6,16 +6,16 @@ var typeOptions = {
   'number': ['Default', 'Tens', 'Hundreds', 'Thousands'],
   'date': ['Default', 'Day', 'Month', 'Year'],
   'time': ['Default', 'Minute', '15 Minutes', 'Half Hour', 'Hour'],
-  'string': ['Default', 'First Letter'],
+  'string': ['Default', 'First Letter', 'Length'],
   'unknown': ['Default']
 }
 
 var functionOptions = {
   'number': ['Count', 'Unique Count', 'Average', 'Maximum', 'Minimum', 'Sum'],
   'date': ['Count', 'Unique Count', 'Most Frequent Month', 'Most Frequent Day'],
-  'time': ['Count', 'Unique Count', 'Most Frequent Hour', 'Most Frequent Half Hour'],
-  'string': ['Count', 'Unique Count', 'Most Frequent String', 'Most Frequent Word'],
-  'unknown': ['Count', 'Unique Count', 'Most Frequent']
+  'time': ['Count', 'Unique Count', 'Most Frequent Hour'],
+  'string': ['Count', 'Unique Count'],
+  'unknown': ['Count', 'Unique Count']
 }
 
 var knownTypes = {
@@ -27,14 +27,36 @@ var knownTypes = {
   'interpreted': 'string',
   'state': 'unknown', 
   'activity': 'string',
-  'name': 'string'
+  'name': 'string',
+  'temperature': 'number',
+  'temp': 'number',
 }
+
+var timeFormat = [{regex: /^(0?[1-9]|1[0-2]):[0-5][0-9]\s*[aApP][mM]?$/gm,format: 'hh:mm a'},
+                  {regex: /^(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/gm, format: 'HH:mm'},
+                  {regex: /^(0?[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}$/gm, format: 'HH:mm:ss'},
+                  {regex: /^(0?[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}\.\d{1,3}$/gm, format: 'HH:mm:ss.SSS'}]
+var dateFormat = []
 
 function getKnownTypeOptions(column) {
   return typeOptions[knownTypes[column]];
 }
 function getFunctionOptions(column) {
   return functionOptions[knownTypes[column]];
+}
+
+function getTimeRegex(value) {
+  var regex, format;
+  for (var index in timeFormat) {
+    regex = timeFormat[index].regex;
+    format = timeFormat[index].format;
+
+    if (value.match(regex)) {
+      console.log(format);
+      return format;
+    }    
+  }
+  return null;
 }
 
 var sliderStitch = document.getElementById("stitchRange");
